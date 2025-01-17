7974f86f75928cdfb3b6b72546f45ef8f3e81576
Eduard Kangash <kangash1996@gmail.com>
2025-01-17 14:26:57 +0300

    First commit

---
+++ packages/index.js
@@ -0,0 +1,6 @@
+import { obj } from "./support/obj.js";
+import { str } from "./support/str.js";
+import { arr } from "./support/arr.js";
+
+
+module.exports = { obj, str, arr };
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import path from "path";
+import { defineConfig } from "vite";
+import packageJson from "./package.json";
+
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.js`,
+  iife: `${getPackageName()}.iife.js`,
+};
+
+const formats = Object.keys(fileName);
+
+export default defineConfig({
+  base: "./",
+  build: {
+    type: "module",
+    outDir: "./build/dist",
+    lib: {
+      entry: path.resolve(__dirname, "/packages/index.js"),
+      name: getPackageNameCamelCase(),
+      formats,
+      fileName: format => fileName[format],
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./")
+      },
+    ],
+  }
+});
